72b7d99910063e76f06738a58ed36fd16a8483c6
hardik <hardikvekariya09@gmail.com>
2022-09-13 11:39:18 +0530

    added stimulus and added crud

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_905_062_701) do
+ActiveRecord::Schema[7.0].define(version: 20_220_912_112_225) do
   create_table 'books', force: :cascade do |t|
     t.string 'name'
     t.decimal 'price'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'description', limit: 200
+    t.boolean 'completed'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
