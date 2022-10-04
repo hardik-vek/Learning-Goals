d09e4eabf79568f61e1928ecdeb4b792b6624efa
hardik-vekariya <hardikvekariya218@gmail.com>
2022-10-04 12:25:07 +0530

    Article crud

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: %i[show edit update destroy]
+
+  def index
+    @articles = ::Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = ::Article.new
+  end
+
+  def create 
+    @article = Article.new(articles_params)
+    if @article.save
+      flash[:notice] = 'Article created successfully'
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(articles_params)
+      flash[:notice] = 'Article updated successfully'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy 
+    @article.destroy
+    redirect_to articles_path
+  end
+  
+  private
+
+  def find_article
+    @article = ::Article.find(params[:id])
+  end
+
+  def articles_params
+    params.require(:article).permit(:title,:description)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_070021) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_053455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer "priority", default: 0, null: false
